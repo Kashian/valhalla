a7ea3b501d6ab835f8d80bc0e7e497ec23a6db87
Alireza Kashian <alireza.kashian@gmail.com>
2023-09-08 18:55:02 -0700

    log more

--- src/sif/truckcost.cc
+++ src/sif/truckcost.cc
@@ -525,7 +525,7 @@ Cost TruckCost::EdgeCost(const baldr::DirectedEdge* edge,
     // Add a penalty for traversing a closed edge
     factor *= closure_factor_;
   }
-  LOG_WARN("Edge cost in seconds: " + std::to_string(sec_cost)+ "    factor: "+ std::to_string(factor));
+  LOG_WARN("Edge cost in seconds: " + std::to_string(sec_cost)+ "    factor: "+ std::to_string(factor)+  " duration" +std::to_string(sec));
 
   return {sec_cost * factor, sec};
 }
